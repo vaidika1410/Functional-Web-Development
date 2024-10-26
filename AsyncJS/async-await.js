34d0313b146df1785987f362677c8a92e3c997d2
vaidika1410 <vaidikakaul20@gmail.com>
2024-10-26 13:56:35 +0530

    smashing async and await in JS

--- AsyncJS/async-await.js
+++ AsyncJS/async-await.js
@@ -5,8 +5,26 @@
 // await keyword is used with async function only
 // by using await keyword, we can replace then() method
 
+
+// without using  async and await
+// function abcd(){
+//     fetch(`https://randomuser.me/api/`)
+//     .then(function(raw){
+//         return  raw.json();
+//     })
+//     .then(function(data){
+//         console.log(data);
+//     }   
+// );
+// }
+
+// abcd();
+
+
 async  function myFunction(){
-    let raw = await fetch(`https://www.google.com`) 
+    let raw = await fetch(`https://randomuser.me/api/`) 
     let ans = await raw.json();  
     console.log(ans)
 }
+
+myFunction(); 
