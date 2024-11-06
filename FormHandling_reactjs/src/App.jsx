369eb0b100e1644d90ee80e04b4bcf8fe1228160
vaidika1410 <vaidikakaul20@gmail.com>
2024-11-06 23:37:19 +0530

    Form handling using Reactjs

--- FormHandling_reactjs/src/App.jsx
+++ FormHandling_reactjs/src/App.jsx
@@ -1,33 +1,31 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import React from 'react'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [username, setUsername] = useState('')
+
+  const submitHandler = (e)=>{
+    e.preventDefault();
+    console.log(username)
+
+    setUsername('')
+  }
 
   return (
     <>
       <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+        <form onSubmit={(e)=>{
+          submitHandler(e)
+        }}>
+          <input 
+          value={username} 
+          onChange={(e)=>{
+            setUsername(e.target.value)
+          }}
+          type="text" name="name" placeholder="Enter your name" />
+          <button type="submit">submit</button>
+        </form>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
   )
 }
